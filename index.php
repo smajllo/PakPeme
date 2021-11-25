e7d2bcabd1728705ef11670d2daddf3fcf811ebc
smajllo <69193718+smajllo@users.noreply.github.com>
2021-11-25 22:11:49 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<title>Hello, world!</title>
+</head>
+<body>
+	<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 px-5">
+		<a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+			<img src="img/logo.svg">
+		</a>
+
+		<ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+			<li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+			<li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
+			<li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
+			<li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
+			<li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+		</ul>
+
+		<div class="col-md-3 text-end">
+			<button type="button" class="btn btn-outline-success me-2">Login</button>
+			<button type="button" class="btn btn-success">Sign-up</button>
+		</div>
+	</header>
+
+	<div class="px-4 py-5 mb-5 text-center text-light hero">
+		<img class="d-block mx-auto mb-4" src="/img/logo2.svg" alt="" width="150" height="150">
+		<h1 class="display-5 fw-bold">A Good Company</h1>
+
+		<button type="button" class="btn btn-light btn-lg mt-4 px-4 gap-3">Donate Now</button>
+
+	</div>
+
+
+	<div class="container-fluid text-center">
+		<h1 class="display-4 border-bottom pb-3">What do we do?</h1>
+
+		<div class="container w-75 px-5 mt-5">
+			<h1 class="display-5">About Us</h1>
+			<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<button type="button" class="btn btn-outline-success px-4 py-2">Learn More</button>
+		</div>
+
+		<div class="container-fluid w-75 px-5 mt-5">
+			<h1 class="display-5">Visions</h1>
+			<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<button type="button" class="btn btn-outline-success px-4 py-2">Learn More</button>
+		</div>
+
+		<div class="row w-75 mx-auto mt-5">
+			<div class="col mt-4 d-none d-lg-block">
+				<img class="img-fluid" src="img/bottle.jpg" width="415px" height="276px">
+			</div>
+			<div class="col text-center text-lg-end">
+				<h1 class="display-5">We collect your plastic</h1>
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+
+		</div>
+
+		<div class="mt-5 bg-light">
+			<div class="row py-3 w-75 mx-auto">
+				<div class="col text-start">
+					<h1 class="display-5">Helping our environment</h1>
+					<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.</p>
+				</div>
+
+				<div class="col mt-4">
+					<img class="img-fluid" src="img/leaf.svg" width="415px" height="276px">
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
